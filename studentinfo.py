45563c0663091a993c399c7f2bf508391c01cd32
AndrewPrymenko <ffeelex@gmail.com>
2023-03-11 11:04:53 +0100

    #added class student

---
+++ studentinfo.py
@@ -0,0 +1,9 @@
+class Student:
+    amount_students = 0
+    def __init__(self, name, age, height):
+        self.height = height
+        self.age = age
+        self.name = name
+        Student.amount_students += 1
+    def GetFullInfo(self):
+        return f"Name: {self.name}\nAge: {self.age}\nHeight: {self.height}"
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import studentinfo as st
+andrii = st.Student("Andrii", 15, 170)
+print(andrii.GetFullInfo())
+print((andrii.amount_students))
+dima = st.Student("Dima", 14, 180)
+print(dima.GetFullInfo())
+print((dima.amount_students))
