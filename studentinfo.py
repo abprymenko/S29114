fd2493575f5a756591645f0a012cd6e9d70c1581
AndrewPrymenko <ffeelex@gmail.com>
2023-03-18 08:23:58 +0100

    #added 3th student

--- studentinfo.py
+++ studentinfo.py
@@ -1,9 +1,9 @@
 class Student:
     amount_students = 0
     def __init__(self, name, age, height):
-        self.height = height
-        self.age = age
         self.name = name
+        self.age = age
+        self.height = height
         Student.amount_students += 1
     def GetFullInfo(self):
         return f"Name: {self.name}\nAge: {self.age}\nHeight: {self.height}\nTotal amount of students: {Student.amount_students}"
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import studentinfo as st
+st.Student.amount_students += 3
+print(f"Total amount of students: {st.Student.amount_students}")
 andrii = st.Student("Andrii", 15, 170)
 print(andrii.GetFullInfo())
 #print((andrii.amount_students))
 dima = st.Student("Dima", 14, 180)
 print(dima.GetFullInfo())
 #print((dima.amount_students))
+roma = st.Student("Roma", 15, 183)
+print(roma.GetFullInfo())
